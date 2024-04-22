8e57ba3c0b4d6fb91d2cf57af30d647d187a21e6
Yarik Ozerskiy <ozerskiy.yaroslav@gmail.com>
2024-04-23 01:03:09 +0400

    add openapi.json and generate services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserModule} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    BrowserModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
